2ba55d8a908f34df9bb727779f14db9dc0e8a808
bongguma <7602kyj@e-mirim.hs.kr>
2021-06-29 11:09:05 +0900

    firebase Analytics 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:sometrend_charttest/Auth/AuthProvider.dart';
 import 'package:sometrend_charttest/BubbleChartExampleView.dart';
@@ -14,6 +15,7 @@ import 'package:sometrend_charttest/RadarChartView.dart';
 import 'package:sometrend_charttest/SyncfusionBubbleChartView.dart';
 
 final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+final FirebaseAnalytics analytics = FirebaseAnalytics();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();    // main 메소드에서 비동기 메소드 사용할 때 반드시 넣어줘야한다.
@@ -38,6 +40,11 @@ class MyApp extends StatelessWidget {
         child:
         MyHomePage(title: 'Flutter Demo Home Page'),
       ),
+      // FirebaseAnalyticsObserver는 GA (google Analytics) 확인을 위해 추가해준 로직으로,
+      // Analytics 라이브러리 또한 pubspec 붙여주어야 라이브러리 사용이 가능
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analytics),
+      ],
     );
   }
 }
