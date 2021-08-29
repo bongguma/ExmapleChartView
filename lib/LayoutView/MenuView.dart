6850879c3a0bcab23724fd7eaf6394fee7d438ee
kimyejin <7602kyj@e-mirim.hs.kr>
2021-08-29 23:27:28 +0900

    refactor: appBar 디자인 수정 및 MenuView 디자인 수정

--- lib/LayoutView/MenuView.dart
+++ lib/LayoutView/MenuView.dart
@@ -1,39 +1,40 @@
+import 'package:get/get.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:kakao_flutter_sdk/all.dart';
-import 'package:provider/provider.dart';
 import 'package:sometrend_charttest/LayoutView/BubbleChartExampleView.dart';
 import 'package:sometrend_charttest/LayoutView/DartGrammerTestView.dart';
-import 'package:sometrend_charttest/Data/CounterProvider.dart';
 import 'package:sometrend_charttest/Data/Type/LoginType.dart';
 import 'package:sometrend_charttest/LayoutView/LineChartExampleView.dart';
 import 'package:sometrend_charttest/LayoutView/MindmapGraphView.dart';
 import 'package:sometrend_charttest/LayoutView/RadarChartView.dart';
 import 'package:sometrend_charttest/LayoutView/SyncfusionBubbleChartView.dart';
 import 'package:sometrend_charttest/LayoutView/defaultLayoutView/DefalutAppBar.dart';
+import 'package:sometrend_charttest/theme/ThemeFactory.dart';
 
 import '../Data/KakaoTalkAccount.dart';
 
 class MenuView extends StatelessWidget {
+
+
+  final LoginType loginType = Get.arguments['loginType'];
+  final KakaoTalkAcounnt kakaoTalkAccount = Get.arguments['kakaoTalkAccount'];
 
   MenuView({Key key}) : super(key : key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: DefaultAppbar(false),
-      body: Menu(),
+      appBar: DefaultAppbar(isBackBtn: false, loginType: loginType,),
+      body: Menu(loginType: loginType, kakaoTalkAccount: kakaoTalkAccount,),
     );
   }
 }
 
 class Menu extends StatefulWidget {
+  final LoginType loginType;
+  final KakaoTalkAcounnt kakaoTalkAccount;
 
-  final LoginType loginType = Get.arguments['loginType'];
-  final KakaoTalkAcounnt kakaoTalkAccount = Get.arguments['kakaoTalkAccount'];
-
-  Menu({Key key}) : super(key : key);
+  Menu({Key key, @required this.loginType, @required this.kakaoTalkAccount}) : super(key : key);
 
   @override
   MenuState createState() => MenuState();
@@ -52,7 +53,7 @@ Widget KakaoProfileView(widget) {
               ? NetworkImage(widget.kakaoTalkAccount
               .profileImageUrl)
               : AssetImage("assets/images/testImage.jpg")),
-      SizedBox(width: 5),
+      SizedBox(width: 10),
       Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -68,102 +69,99 @@ Widget KakaoProfileView(widget) {
 class MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
+    var theme = new ThemeFactory.of(context).theme;
+
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            widget.loginType == LoginType.KAKAO ? KakaoProfileView(widget) : Container(),
-            Flexible(
-              flex: 1,
-              child: Column(
-                children: [
-                  ElevatedButton(
-                    child: Text('라인차트 예제'),
-                    onPressed: () {
-                      print('loginType :: ' + widget.loginType.toString());
-                      // chart 라이브러리 사용해서 예제 진행-
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => LineChartExampleView()));
-                    },
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  ElevatedButton(
-                    child: Text('버블차트 예제'),
-                    onPressed: () {
-                      // chart 라이브러리 사용해서 예제 진행-
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => BubbleChartExampleView()));
-                    },
-                  ),
-                  ElevatedButton(
-                    child: Text('Syncfusion 차트 예제'),
-                    onPressed: () {
-                      // chart 라이브러리 사용해서 예제 진행-
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => SyncfusionBubbleChartView()));
-                    },
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  ElevatedButton(
-                    child: Text('radar 차트 예제'),
-                    onPressed: () {
-                      // chart 라이브러리 사용해서 예제 진행-
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => RadarChartView()));
-                      // 메소드 실행을 위해 Notify를 받지 않는 Provider of를 지정
-                    },
-                  ),
-                  ElevatedButton(
-                    child: Text('google 로그인예제'),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => DartGrammerTestView()));
-                    },
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  ElevatedButton(
-                    child: Text('mindmap 그래프 예제'),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => MindmapGraphView()));
-                    },
-                  ),
-                  ElevatedButton(
-                    child: Text('로그아웃'),
-                    onPressed: () async {
-                      if(widget.loginType == LoginType.KAKAO) {
-                        await UserApi.instance.logout();
-                        Navigator.pop(context, false);
-                      }
-                    },
-                  ),
-                ],
+      backgroundColor: theme.secondaryBgColor,
+      body: Padding(
+        padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 10.0, bottom: 10.0),
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              widget.loginType == LoginType.KAKAO ? KakaoProfileView(widget) : Container(),
+              Flexible(
+                flex: 1,
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      child: Text('라인차트 예제'),
+                      onPressed: () {
+                        print('loginType :: ' + widget.loginType.toString());
+                        // chart 라이브러리 사용해서 예제 진행-
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LineChartExampleView()));
+                      },
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(
+                      child: Text('버블차트 예제'),
+                      onPressed: () {
+                        // chart 라이브러리 사용해서 예제 진행-
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => BubbleChartExampleView()));
+                      },
+                    ),
+                    ElevatedButton(
+                      child: Text('Syncfusion 차트 예제'),
+                      onPressed: () {
+                        // chart 라이브러리 사용해서 예제 진행-
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SyncfusionBubbleChartView()));
+                      },
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(
+                      child: Text('radar 차트 예제'),
+                      onPressed: () {
+                        // chart 라이브러리 사용해서 예제 진행-
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => RadarChartView()));
+                        // 메소드 실행을 위해 Notify를 받지 않는 Provider of를 지정
+                      },
+                    ),
+                    ElevatedButton(
+                      child: Text('google 로그인예제'),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => DartGrammerTestView()));
+                      },
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(
+                      child: Text('mindmap 그래프 예제'),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MindmapGraphView()));
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            ],
+          ),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      );
   }
 }
--- lib/LayoutView/defaultLayoutView/DefalutAppBar.dart
+++ lib/LayoutView/defaultLayoutView/DefalutAppBar.dart
@@ -1,15 +1,16 @@
 
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/all.dart';
+import 'package:sometrend_charttest/Data/Type/LoginType.dart';
+import 'package:sometrend_charttest/theme/ThemeFactory.dart';
 
 class DefaultAppbar extends StatefulWidget implements PreferredSizeWidget {
 
-  DefaultAppbar(bool isBackBtn, {Key key}) : isBackBtn = isBackBtn, preferredSize = Size.fromHeight(100), super(key: key);
-
-  @override
   final bool isBackBtn;
+  final LoginType loginType;
+  final Size preferredSize = Size.fromHeight(50);
 
-  @override
-  final Size preferredSize;
+  DefaultAppbar({Key key, @required this.isBackBtn, @required this.loginType}) : super(key: key);
 
   @override
   State<DefaultAppbar> createState() => _defaultAppbarState();
@@ -25,23 +26,31 @@ class _defaultAppbarState extends State<DefaultAppbar> {
 
   /* backIcon Btn을 그리는 Widget */
   Widget backIconBtn() {
-    return IconButton(
-        icon: Icon(
-          Icons.arrow_back,
-          color: Colors.black,
-        ),
-        onPressed: () {
-
-        });
+    return GestureDetector(
+      child: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+      ),
+      onTap: () async {
+        print('backIconBtn!!!!!!!!!');
+        if(widget.loginType == LoginType.KAKAO) {
+          await UserApi.instance.logout();
+          Navigator.pop(context, false);
+        }
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    var theme = new ThemeFactory.of(context).theme;
 
     return DefaultTabController(
       length: 5,
       child: AppBar(
-        backgroundColor: Colors.red,
+        backgroundColor: theme.secondaryBgColor,
         elevation: 0.0,
         automaticallyImplyLeading: false,
         leading: widget.isBackBtn ? backIconBtn() : Container(),
@@ -52,7 +61,7 @@ class _defaultAppbarState extends State<DefaultAppbar> {
               width: 30,
               child: Icon(
                 Icons.lock,
-                color: Colors.black,
+                color: theme.primaryBlackBgColor,
               ),
             ),
           ],
